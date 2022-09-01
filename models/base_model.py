d051185ea6ab8fc8c611d18e396182be4e6e33a7
Nobby-code <norbertguda0@gmail.com>
2022-09-01 05:26:21 -0700

    dictionary recreation - dealing with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,18 +8,38 @@ import uuid
 import models
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """ initialization method """
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
+        """ printing string formated output """
         return f"[{ self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ saves the object """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ Returning a dictionary format """ 
         my_dict = self.__dict__
         my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
